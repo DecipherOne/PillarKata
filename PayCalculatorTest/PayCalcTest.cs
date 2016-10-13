325302f72c8ab98fd3cbf26badbb30fa85ac0547
Will <willc@decipherone.com>
2016-10-13 15:56:59 -0500

    added endtimeverrification test

--- PayCalculatorTest/PayCalcTest.cs
+++ PayCalculatorTest/PayCalcTest.cs
@@ -29,5 +29,14 @@ namespace PayCalculatorTest
             myCalc.bedTime[2] = 00;
             Assert.IsTrue(myCalc.bedTime[0] > 0 && myCalc.bedTime[0] >= 1 && myCalc.bedTime[0] < 13);
         }
+
+        [TestMethod]
+        public void VerifyEndTimeIsValid()
+        {
+            myCalc.endTime[0] = 7;
+            myCalc.endTime[1] = 00;
+            myCalc.endTime[2] = 00;
+            Assert.IsTrue(myCalc.endTime[0] > 0 && myCalc.endTime[0] >= 1 && myCalc.endTime[0] < 13);
+        }
     }
 }
