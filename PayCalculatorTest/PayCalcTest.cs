b0e990505c92d4a00c6ae7e1034bfbae8b2f491b
Will <willc@decipherone.com>
2016-10-13 15:54:44 -0500

    added bedtime verrification test

--- PayCalculatorTest/PayCalcTest.cs
+++ PayCalculatorTest/PayCalcTest.cs
@@ -18,8 +18,16 @@ namespace PayCalculatorTest
             myCalc.startTime[0] = 10;
             myCalc.startTime[1] = 00;
             myCalc.startTime[2] = 00;
-            Assert.IsTrue(myCalc.startTime[0] > 5 && myCalc.startTime[0] < 13);
-                        
+            Assert.IsTrue( myCalc.startTime[0] > 0 && myCalc.startTime[0] >= 5 && myCalc.startTime[0] < 13);   
+        }
+
+        [TestMethod]
+        public void VerifyBedTimeIsValid()
+        {
+            myCalc.bedTime[0] = 7;
+            myCalc.bedTime[1] = 00;
+            myCalc.bedTime[2] = 00;
+            Assert.IsTrue(myCalc.bedTime[0] > 0 && myCalc.bedTime[0] >= 1 && myCalc.bedTime[0] < 13);
         }
     }
 }
