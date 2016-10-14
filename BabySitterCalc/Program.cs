f04790f32545bceaaa78c6ff6386383d1f3e42e7
Will <willc@decipherone.com>
2016-10-14 14:35:20 -0500

    Finished up Program.cs

--- BabySitterCalc/Program.cs
+++ BabySitterCalc/Program.cs
@@ -8,15 +8,18 @@ namespace BabySitterCalc
 {
     class Program
     {
-      
+
         static void Main(string[] args)
         {
-            PrintHeader();
+            PayCalculator myCalc = new PayCalculator();
+            GetUserStartTime(myCalc);
+            GetUserBedTime(myCalc);
+            GetUserEndTime(myCalc);
+            ShowAmountDo(myCalc);
 
-            PrintFooter();
         }
 
-       static void PrintHeader()
+        static void PrintHeader()
         {
             string head = "($$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$)";
             head += "\n \t          Baby Sitter Calculator";
@@ -26,12 +29,162 @@ namespace BabySitterCalc
             Console.Write(head);
         }
 
-       static void PrintFooter()
+        static void PrintFooter()
         {
-            string head = "($$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$)";
-            head += "\n          By Baby Co, where your baby is our business.";
-            head += "\n($$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$) \n";
-            Console.Write(head);
+            string foot = "($$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$)";
+            foot += "\n          By Baby Co, where your baby is our business.";
+            foot += "\n($$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$) \n \n";
+            Console.Write(foot);
+
+        }
+
+        static void GetUserStartTime(PayCalculator myCalc)
+        {
+            bool done = false;
+            while (!done)
+            {
+                PrintHeader();
+                Console.Write("\n \n \t Please Enter the time you begain your job. \n \t Example Format - 5:21 \n \n");
+                PrintFooter();
+                string buffer = null;
+                buffer = Console.ReadLine();
+
+                int p = buffer.IndexOf(':');
+                if (p > 0)
+                {
+                    int hour = 0;
+                    int min = 0;
+                    try
+                    {
+                        string h = null;
+                        string m = null;
+
+                        h = buffer.Substring(0, p);
+                        hour = Convert.ToInt32(h);
+                        int c = p + 1;
+                        m = buffer.Substring(c, 2);
+                        min = Convert.ToInt32(m);
+                    }
+                    catch (InvalidCastException e)
+                    {
+                        Console.WriteLine("Ivalid cast exception : %s", e);
+                    }
+
+                    myCalc.SetStartTime(hour, min, 0);
+                    done = true;
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.Write(" \n \n \t The format you entered is not valid press any key to try again.");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+
+        }
+
+        static void GetUserBedTime(PayCalculator myCalc)
+        {
+            bool done = false;
+            while (!done)
+            {
+                PrintHeader();
+                Console.Write("\n \n \t Please Enter the time you placed the child to be \n \t Example Format - 5:21 \n \n");
+                PrintFooter();
+                string buffer = null;
+                buffer = Console.ReadLine();
+
+                int p = buffer.IndexOf(':');
+                if (p > 0)
+                {
+                    int hour = 0;
+                    int min = 0;
+                    try
+                    {
+                        string h = null;
+                        string m = null;
+
+                        h = buffer.Substring(0, p);
+                        hour = Convert.ToInt32(h);
+                        int c = p + 1;
+                        m = buffer.Substring(c, 2);
+                        min = Convert.ToInt32(m);
+                    }
+                    catch (InvalidCastException e)
+                    {
+                        Console.WriteLine("Ivalid cast exception : %s", e);
+                    }
+
+                    myCalc.SetBedTime(hour, min, 0);
+                    done = true;
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.Write(" \n \n \t The format you entered is not valid press any key to try again.");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+        }
+
+        static void GetUserEndTime(PayCalculator myCalc)
+        {
+            bool done = false;
+            while (!done)
+            {
+                PrintHeader();
+                Console.Write("\n \n \t Please Enter the time your job ended. \n \t Example Format - 5:21 \n \n");
+                PrintFooter();
+                string buffer = null;
+                buffer = Console.ReadLine();
+
+                int p = buffer.IndexOf(':');
+                if (p > 0)
+                {
+                    int hour = 0;
+                    int min = 0;
+                    try
+                    {
+                        string h = null;
+                        string m = null;
+
+                        h = buffer.Substring(0, p);
+                        hour = Convert.ToInt32(h);
+                        int c = p + 1;
+                        m = buffer.Substring(c, 2);
+                        min = Convert.ToInt32(m);
+                    }
+                    catch (InvalidCastException e)
+                    {
+                        Console.WriteLine("Ivalid cast exception : %s", e);
+                    }
+
+                    myCalc.SetEndTime(hour, min, 0);
+                    done = true;
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.Write(" \n \n \t The format you entered is not valid press any key to try again.");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+        }
+
+
+        public static void ShowAmountDo(PayCalculator myCalc)
+        {
+            PrintHeader();
+            myCalc.CalculateNightsPay();
+            Console.WriteLine("   Calculated start to bed ay : {0}", myCalc.startToBedPay);
+            Console.WriteLine("   Calculated bed to midnight pay : {0}", myCalc.bedToMidnightPay);
+            Console.WriteLine("   Calculated midnight to end of night pay: {0}", myCalc.midnightToEndPay);
+            Console.WriteLine("   Final Amount Owed : {0}", myCalc.fullPay);
+            Console.WriteLine("\n \n Thank you for using our Calculator, Press any key to exit.");
+            PrintFooter();
             Console.ReadKey();
         }
     }
